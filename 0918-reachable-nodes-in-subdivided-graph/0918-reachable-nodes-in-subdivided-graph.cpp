88c6e0636758df2a1fa794d00c9d4787d7298a32
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2024-10-11 20:30:59 +0300

    Time: 965 ms (5.03%), Space: 64.2 MB (25.14%) - LeetHub

---
+++ 0918-reachable-nodes-in-subdivided-graph/0918-reachable-nodes-in-subdivided-graph.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int reachableNodes(vector<vector<int>>& edges, int maxMoves, int n) {
+        int N = n;
+        vector<array<int, 2>> adj[N];
+        unordered_map<int, unordered_map<int, int>> mp;
+        for(auto &e: edges){
+            int u = e[0], v = e[1], w = e[2] + 1;
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+            mp[u][v] = mp[v][u] = e[2];
+        }
+
+        vector<int> dist(N, INT_MAX);
+        vector<bool> visited(N);
+        dist[0] = 0;
+        priority_queue<array<int,2>, vector<array<int,2>>, greater<>> pq;
+        pq.push({0, 0});
+        while(!pq.empty()){
+            auto [d, v] = pq.top(); pq.pop();
+            if(visited[v]) continue;
+            visited[v] = true;
+            for(auto [u, w]: adj[v]){
+                int new_d = d + w;
+                if(new_d < dist[u]){
+                    dist[u] = new_d;
+                    pq.push({new_d, u});
+                }
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0; i < N; ++i){
+            if(dist[i] <= maxMoves) ans++;
+            for(int j = i + 1; j < N; ++j){
+                if(mp[i].count(j) == 0) continue;
+                int cnti = mp[i][j], n1 = max(0, maxMoves - dist[i]), n2 = max(0, maxMoves - dist[j]);
+                int cur_ans = min(cnti, n1 + n2);
+                ans += cur_ans;
+            }
+        }
+        return ans;
+    }
+};
