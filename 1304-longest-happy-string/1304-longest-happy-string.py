2c180efbda0834bcb178f89d2374862f1955df6f
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2024-10-17 01:41:30 +0300

    Time: 35 ms (58.79%), Space: 16.7 MB (29.72%) - LeetHub

---
+++ 1304-longest-happy-string/1304-longest-happy-string.py
@@ -0,0 +1,78 @@
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        ans = []
+        heap = []
+        heapq.heappush(heap, (-1* a, "a"))
+        heapq.heappush(heap, (-1 *b, "b"))
+        heapq.heappush(heap, (-1 *c, "c"))
+        while c != 0 or b != 0 or a != 0:
+            # while heap: 
+            val, letter = heapq.heappop(heap)
+            curr = letter
+
+            if len(ans) >= 2:
+                if ans[-1] == letter and ans[-2] == letter:
+                    if heap:
+                        val2, letter2 = heapq.heappop(heap)
+                        print("ans2", val2, letter2)
+                        curr = letter2
+                        heapq.heappush(heap, (val, letter))
+                        val = val2
+                        if val2 == 0:
+                            break   
+                    else:
+                        break             
+                    
+
+            # fl = True
+            print("ans", val, letter)
+            print("curr", curr)
+            if c != 0 and curr == "c":
+                if len(ans) <= 1:
+                    ans.append("c")
+                    c-=1
+                    heapq.heappush(heap, (val+1, curr))
+                    continue
+                else:
+                    if ans[-1] == "c" and ans[-2] == "c":
+                        pass
+                    else:
+                        ans.append("c")
+                        c-=1
+                        heapq.heappush(heap, (val+1, curr))
+
+                        continue
+            if b != 0 and curr == "b":
+                if len(ans) <=1:
+                     ans.append("b")
+                     b -=1
+                     heapq.heappush(heap, (val+1, curr))
+
+                     continue
+                else:
+                    if ans[-1] == "b" and ans[-2] == "b":
+                        pass
+                    else:
+                        ans.append("b")
+                        b -=1
+                        heapq.heappush(heap, (val+1, curr))
+
+                        continue
+            if a != 0 and curr == "a":
+                if len(ans) <=1:
+                     ans.append("a")
+                     a -=1
+                     heapq.heappush(heap, (val+1, curr))
+                     continue
+                else:
+                    if ans[-1] == "a" and ans[-2] == "a":
+                        pass
+                    else:
+                        ans.append("a")
+                        a -=1
+                        heapq.heappush(heap, (val+1, curr))
+                        continue
+            # if fl:
+            #     break
+        return "".join(ans)
+                    
