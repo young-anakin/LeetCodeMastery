51e54ac036ab768e68613c2ac80c1a04b8ab61a5
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2024-10-16 23:42:03 +0300

    Time: 433 ms (29.81%), Space: 24.2 MB (13.38%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
@@ -1,39 +1,17 @@
 class Solution(object):
     def dividePlayers(self, skill):
-        """
-        :type skill: List[int]
-        :rtype: int
-        """
-        skill.sort()
-        cp = Counter(skill)
-        def is_not_whole_number(num):
-            return num % 1 != 0
-
-        sm = sum(skill)
-        under = len(skill)/2
-        div = sm/under
-        print(sm, under, div)
-        fl = True
-        if sm%2 != 0 and under %2 != 1:
-            print("fa")
-            return -1
-        
-        if sm%div != 0:
-            print("saaa")
-            return -1
-
-        ans = 0
-        for ind in range(len(skill)/2):
-            # print(skill[ind], div-skill[ind])
-            cp[skill[ind]] -=1
-            if cp[div-skill[ind]] > 0:
-                cp[div-skill[ind]] -=1
-                ans += skill[ind] * (div-skill[ind])
-            else:
-                print("jja")
-                return -1
-        return ans
-
-
+        sortedSkill = sorted(skill)
+        ass = []
+        ass2 = 0
+        ptr1 = 0
+        ptr2 = len(skill)-1
+        for a in range(0, int(len(sortedSkill)/2)):
+            ass.append(sortedSkill[ptr1] + sortedSkill[ptr2])
+            ass2 += sortedSkill[ptr1] * sortedSkill[ptr2]
+            ptr1 += 1
+            ptr2 -= 1
+        if ass.count(ass[0]) == len(ass):
+            return ass2
+        return -1
 
         
