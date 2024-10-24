02e43b7a621c9672a9baf42dbde8741cf615a0c7
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2024-10-24 23:57:16 +0300

    Time: 24 ms (97.51%), Space: 18.7 MB (95.91%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -2,12 +2,10 @@ class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
         nums.sort()
         sm = 0
-        fl = True
+        # fl = True
         for ind in range(0, len(nums)):
-            if fl:
+            if ind%2 == 0:
                 sm += nums[ind]
-                fl = False
-            else:
-                fl = True
+
         
         return sm
