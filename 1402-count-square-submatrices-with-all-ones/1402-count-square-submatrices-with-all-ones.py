3e9b2918f82034ae98cc6d3520d2dfe4612836bb
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2024-10-28 01:06:13 +0300

    Time: 54 ms (90.12%), Space: 19.5 MB (15.81%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
@@ -3,25 +3,23 @@ class Solution:
         rows = len(matrix)
         cols = len(matrix[0])
 
+        nums = matrix
+
         # Create a DP table with the same dimensions as the matrix
         dp = [[0] * cols for _ in range(rows)]
 
-        # Variable to store the total number of square submatrices
-        total_squares = 0
+        tot = 0
 
-        # Iterate over each cell in the matrix
-        for i in range(rows):
+        for ind in range(rows):
             for j in range(cols):
-                # If the current cell is 1, check for square possibilities
-                if matrix[i][j] == 1:
-                    # If we're in the first row or column, just copy the value
-                    if i == 0 or j == 0:
-                        dp[i][j] = 1
+                if nums[ind][j] == 1:
+                    if ind == 0 or j == 0:
+                        dp[ind][j] = 1
+                    
                     else:
-                        # Use the min value of the top, left, and diagonal-up-left cells + 1
-                        dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+                        dp[ind][j] = min(dp[ind][j-1], dp[ind-1][j-1], dp[ind-1][j]) + 1
+                    
 
-                    # Add the number of squares ending at this cell to the total count
-                    total_squares += dp[i][j]
-
-        return total_squares
+                    tot += dp[ind][j]
+        
+        return tot
