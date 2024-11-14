92571a4622c6bfcabf035a9fd1a1adc9bb725f98
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2024-11-14 03:35:19 +0300

    Time: 265 ms (41.57%), Space: 31.2 MB (59.82%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
@@ -1,22 +1,22 @@
-from bisect import bisect_left, bisect_right
-from typing import List
-
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+
+        
         nums.sort()
+        # 0 1 4 4 5 7
         ans = 0
-
-        for ind in range(len(nums) - 1):
-            # Calculate the range of values to find pairs within the bounds
+        print(nums)
+        for ind in range(len(nums)-1):
+            x = nums[ind]
             minimumOption = lower - nums[ind]
+            
             maximumOption = upper - nums[ind]
+            mn = bisect_left(nums, minimumOption, ind+1)
+            mx = bisect_right(nums, maximumOption, ind+1)-1
 
-            # Use bisect_left and bisect_right with the adjusted start index to avoid slicing
-            mn = bisect_left(nums, minimumOption, ind + 1)
-            mx = bisect_right(nums, maximumOption, ind + 1) - 1
-
-            # If valid indices are found within bounds, count the pairs
             if mn <= mx:
-                ans += mx - mn + 1
-
+                if ind + mn == ind:
+                    ans += abs(mn - mx)
+                else:
+                    ans += abs(mx - mn)+1
         return ans
