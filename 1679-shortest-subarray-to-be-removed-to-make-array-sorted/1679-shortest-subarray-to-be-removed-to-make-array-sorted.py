101b7b32ffe92b147a4a60dd24335ffd5d09d546
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2024-11-15 23:13:30 +0300

    Time: 27 ms (26.09%), Space: 31.3 MB (24.07%) - LeetHub

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/1679-shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -1,27 +1,18 @@
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
-        prefix = []
-        suffix = []
-
-        stack = []
-        stack.append(arr[0])
+        stack = [arr[0]]
         start = 1
-        mn = 1
         beg = stack
 
         while start < len(arr):
             if arr[start] >= stack[-1]:
                 stack.append(arr[start])
-                if mn <= len(stack):
-                    mn = max(mn, len(stack))
-
-                    beg = stack
+                beg = stack
             else:
                 break
             start +=1
         
         suf = [arr[-1]]
-
         start = 2
 
         while start <= len(arr):
@@ -30,26 +21,19 @@ class Solution:
             else:
                 break
             start +=1
-            # print(suf, start)
+        
         suf = suf[::-1]
-        # print(suf)
-        # print(beg)
+
         middle = len(arr) - (len(suf) + len(beg))
 
-        ans = 0
         ans = min(len(arr) - len(suf), len(arr) - len(beg))
 
-
-        # print(ans)
-        # ans = middle
         if len(suf) == len(beg) == len(arr):
             return 0
         else:
             for ind, val in enumerate(beg):
                 x = bisect_left(suf, val)
                 extra = x + ((len(beg) - ind)-1) + middle
-                # print("Extra", extra)
                 ans = min(ans,  extra)
-                # print(val,x )
         
         return ans
