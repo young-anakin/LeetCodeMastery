259481eb1a47cb030ed9b50df003fc224e5aa4af
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2025-01-07 00:50:23 +0300

    Time: 55 ms (20.91%), Space: 19.1 MB (13.83%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -10,69 +10,26 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-
-        arr = []
-        #left = True
-        xx = root
         def bfs(parent, node, ind, flag):
             if node == None:
                 return
-            
-            # print(node.val)
-
-
-
             if ind == 0:
                 pass
             elif flag:
-                # print("hiiii", node.val)
                 node.next = parent.right
-                # print(node.next.val)
             else:
-                # print("byeeee", node.val)
                 if parent.next == None:
                     node.next = None
                 else:
                     node.next = parent.next.left
-                    # print("hiiii", node.val)
-                    # print(node.next.val)
             
             bfs(node, node.left, ind+1, True)
             bfs(node, node.right, ind+1, False)
 
-        def dfs(parent, node, ind, flag):
-                    if node == None:
-                        return
-                    
-                    # print(node.val)
-
-                    bfs(node, node.left, ind+1, True)
-                    bfs(node, node.right, ind+1, False)
-
-                    if ind == 0:
-                        pass
-                    elif flag:
-                        print("hiiii", node.val)
-                        # node.next = parent.right
-                        # print(node.next.val)
-                    else:
-                        # print("byeeee", node.val)
-                        if parent.next == None:
-                            node.next = None
-                        else:
-                            node.next = parent.next.left
-                            # print("hiiii", node.val)
-                            # print(node.next.val)
-
-        
+        bfs(None, root, 0, False)
 
 
-        
-        bfs(None, xx, 0, False)
-        # dfs(None, xx, 0, False)
-
-
-        return xx
+        return root
 
 
             
