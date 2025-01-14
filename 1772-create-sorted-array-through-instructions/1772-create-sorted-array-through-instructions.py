3e4539090c4d19a728fce2ac62dd7a50e9d180c4
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2025-01-14 17:11:30 +0300

    Time: 3099 ms (37.7%), Space: 32 MB (57.38%) - LeetHub

--- 1772-create-sorted-array-through-instructions/1772-create-sorted-array-through-instructions.py
+++ 1772-create-sorted-array-through-instructions/1772-create-sorted-array-through-instructions.py
@@ -1,21 +1,17 @@
-from sortedcontainers import SortedList
-from typing import List
-
 class Solution:
     def createSortedArray(self, instructions: List[int]) -> int:
         arr = SortedList()
         sm = 0
-        mod = 10**9 + 7
+        fin = ((10 ** 9) + 7)
+        for ind in range(len(instructions)):
+            x = arr.bisect_left(instructions[ind])
+            y = len(arr) - arr.bisect_left(instructions[ind] + 1)
+            arr.add(instructions[ind])
+            # print(x, y)
+            # print(arr)
+            # print(x, y)
+            sm += (min(x, y))
+            sm %= fin
+
         
-        for num in instructions:
-            # Count elements less than `num`
-            left_count = arr.bisect_left(num)
-            # Count elements greater than `num`
-            right_count = len(arr) - arr.bisect_right(num)
-            # Add `num` to the sorted list
-            arr.add(num)
-            # Add the minimum cost to the total sum
-            sm += min(left_count, right_count)
-            sm %= mod
-        
-        return sm
+        return sm 
