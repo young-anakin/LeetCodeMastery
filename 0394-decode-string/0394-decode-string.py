94a4c7d964344c7d70b219efc7727397c81708a0
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2025-03-31 00:39:50 +0300

    Time: 0 ms (100%), Space: 17.8 MB (56.05%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,36 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        # if s == "3[a]2[bc]":
+        #     return "jjo"
+        stack = []
+        fin = ""
+        nums = "1234567890"
+        for i in range(len(s)):
+            if s[i] == "[":
+                print(i, s[i])
+                stack.append(s[i])
+            elif s[i] == "]":
+                print("orig", stack)
+                repeat = []
+                value = []
+                while stack[-1] != "[":
+                    value.append(stack.pop())
+                    # stack.pop()
+                value = value[::-1]
+                stack.pop()
+                print(stack)
+                while stack and stack[-1] in nums:
+                    repeat.append(stack.pop())
+                repeat = repeat[::-1]
+
+                print("Repeat and val", repeat, value)
+                repeat = "".join(repeat)
+                for x in range(int(repeat)):
+                    stack.append("".join(value))
+                print("changed", stack)
+                # for x in tmp:
+                #     stack.append(x)
+            else:
+                stack.append(s[i])
+        return "".join(stack)
+        # print(stack)
