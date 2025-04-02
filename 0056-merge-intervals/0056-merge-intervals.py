a72d59760b8dca1ca8cfc045414b190a6dc718ac
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2025-04-02 04:14:58 +0300

    Time: 31 ms (5.45%), Space: 21.8 MB (15.03%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,37 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+
+        curr = 0
+        flag = False
+        mn, mx = 0, 0
+        ans = []
+        while curr < len(intervals):
+            print(intervals[curr])
+
+            if flag == False:
+                # print("False")
+                flag = True
+                mn = intervals[curr][0]
+                mx = intervals[curr][1]
+            else:
+                curr_a, curr_b = intervals[curr][0], intervals[curr][1]
+
+                if curr_a > mn and curr_a > mx:
+                    # flag = False
+                    ans.append([mn, mx])
+                    mn = intervals[curr][0]
+                    mx = intervals[curr][1]
+                else:
+                    mn = min(curr_a, mn)
+                    mx = max(curr_b, mx)
+                print("True", mn, mx)
+                
+            curr +=1
+        
+        if flag:
+            ans.append([mn, mx])
+        
+        return ans
+
+
