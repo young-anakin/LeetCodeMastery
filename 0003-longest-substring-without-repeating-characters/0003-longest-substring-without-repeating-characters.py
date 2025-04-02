201000a11aa25ae6778bb601335562430ac64771
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2025-04-02 04:27:40 +0300

    Time: 27 ms (23.22%), Space: 18.1 MB (12.63%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,19 +1,27 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        dd = defaultdict(int)
         
-        if len(s) == 0:
-            return 0
-        i = 0
-        mx = 0
-        for x, ind in enumerate(s):
-            dd[ind] +=1
-            while dd[ind] != 1:
-                dd[s[i]] -=1
-                i +=1
+        # This dictionary will store letterCounts for me
+
+        letterCount = defaultdict(int)
+
+
+        # Approach : I'll iterate over the string adding letter by letter into my dictionary. If I encounter a character that has a value more than 1, then that's the longest substring I've reached so far. I'll have to decrement from the start of my string upto which that letter becomes 1. 
+
+        standBy, leader = 0, 0
+        finishLine = len(s)
+        maxSubStr = 0
+        while leader < finishLine:
+
+            letterCount[s[leader]] +=1
+
+            while letterCount[s[leader]] > 1:
+                letterCount[s[standBy]] -=1
+                standBy +=1
             
-            mx = max(mx, abs(x - i))
+            # maximum sub string is going to be the distance between the leader and the stand by guy
+            leader +=1
+            # print(standBy, leader, maxSubStr)
+            maxSubStr = max(maxSubStr, abs(standBy - leader))
         
-        return mx+1
-            
-                
+        return maxSubStr
