89b569024057023af87f2657044c5c68c1ca71b5
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2025-04-02 04:49:57 +0300

    Time: 51 ms (36.89%), Space: 21.7 MB (84%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,21 +1,29 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
+        # I'll use a tuple based stack that tracks each element with how many times it has appeared. If it happens to have the same value as the previous element, we merge them together and increase their count!
+
+
         stack = []
-        for ind in s:
+        i = 0
+
+
+        for i in range(len(s)):
             if not stack:
-                stack.append((ind, 1))
+                stack.append((s[i], 1))
             else:
-                if stack[-1][0] == ind:
-                    a, b = stack.pop()
-                    stack.append((a, b+1))
+                if stack[-1][0] == s[i]:
+                    x, y = stack.pop()
+                    stack.append((x, y + 1))
                 else:
-                    stack.append((ind, 1))
+                    stack.append((s[i], 1))
             
             if stack[-1][1] == k:
                 stack.pop()
-
         
+        # print(stack)
+
         ans = ""
-        for a, b in stack:
-            ans += a*b
+        for key, val in stack:
+            ans += key * val
+        
         return ans
