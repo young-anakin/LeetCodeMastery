73e54de9e18d1dacfcdc27b935dac0b22e459334
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2025-04-03 00:07:45 +0300

    Time: 0 ms (100%), Space: 19.7 MB (68.51%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -7,41 +7,23 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
-        fl = True
-        def bfs(node):
-            nonlocal fl
-            # if node == None:
-            #     return 
-            if node.left == None and node.right == None:
-                return (node.val, node.val)
+        def bfs(node, lowerBound, upperBound):
+            if node == None:
+                return True
             
-
-            if node.left and node.right:
-                x = bfs(node.left)
-                y = bfs(node.right)
-                if (node.val <= x[0] or node.val <= x[1]):
-                    fl = False
-                
-                if (node.val >= y[0] or node.val >= y[1]):
-                    fl = False
-                return (min(x[0], x[1], y[0], y[1]), max(x[0], x[1], y[0], y[1]))
-
-                # pass
-            elif node.left and not node.right:
-                y = bfs(node.left)
-
-                if (node.val <= y[0] or node.val <= y[1]):
-                    fl = False
-                return (min(node.val, y[0]), max(node.val, y[1]))
-            elif node.right and not node.left:
-                y = bfs(node.right)
-
-                if (node.val >= y[0] or node.val >= y[1]):
-                    fl = False
-                return (min(node.val, y[0]), max(node.val, y[1]))
+            if node.val <= lowerBound or node.val >= upperBound:
+                return False
             
+            
+            # let's traverse to the left
 
+            a =  bfs(node.left, lowerBound, node.val)
 
+            # let's traverse to the right
 
-        bfs(root)
-        return (fl)
+            b =  bfs(node.right, node.val, upperBound)
+
+            return (a and b)
+        
+        return bfs(root, float("-inf"), float("inf"))
+        
