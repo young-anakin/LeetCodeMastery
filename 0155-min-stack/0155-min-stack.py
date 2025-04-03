ab5aeafbaada158962715d28e5bbdebfb288ed2e
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2025-04-03 22:55:17 +0300

    Time: 3 ms (84.04%), Space: 21.3 MB (90.9%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,27 +1,34 @@
 class MinStack:
 
     def __init__(self):
-        # self.stack = []
-        self.stack = [(float("inf"), float("inf"))]
-        
+        # this array simulates our stack
+        self.stack = []
+        # this array will be used to get the work for the minimum element
+        self.minstack = []
+
 
     def push(self, val: int) -> None:
-        # x = self.stack[-1]
-        self.stack.append((val, min(val, self.stack[-1][1])))
-        
+        self.stack.append(val)
+        if not self.minstack:
+            self.minstack.append(val)
+        elif val <= self.minstack[-1]:
+            self.minstack.append(val)
 
     def pop(self) -> None:
-        self.stack.pop()
-
+        if self.stack[-1] == self.minstack[-1]:
+            self.stack.pop()
+            self.minstack.pop()
+        else:
+            self.stack.pop()
         
 
     def top(self) -> int:
-        return self.stack[-1][0]
+
+        return self.stack[-1]
         
 
     def getMin(self) -> int:
-        return self.stack[-1][1]
-        
+        return self.minstack[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
