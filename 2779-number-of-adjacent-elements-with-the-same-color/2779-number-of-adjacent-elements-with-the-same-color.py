7de32b699347dee6bb0bae1f30e840a42f078777
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2025-04-28 02:48:59 +0300

    Time: 95 ms (68.24%), Space: 50.1 MB (42.4%) - LeetHub

--- 2779-number-of-adjacent-elements-with-the-same-color/2779-number-of-adjacent-elements-with-the-same-color.py
+++ 2779-number-of-adjacent-elements-with-the-same-color/2779-number-of-adjacent-elements-with-the-same-color.py
@@ -1,36 +1,35 @@
 class Solution:
     def colorTheArray(self, n: int, queries: List[List[int]]) -> List[int]:
-        array = [0 for _ in range(n)]
-        if n == 1:
-            return [0 for _ in range(len(queries))]
-        cp = 0
         ans = []
-        for i, j in queries:
-
-            if i == 0:
-                if array[i+1] == array[i] and (array[i+1] != 0):
+        colors = [0 for _ in range(n)]
+        cp = 0
+        if n == 1:
+            return [0] * len(queries)
+        for ind, val in queries:
+            if ind == 0:
+                if colors[ind] == colors[ind+1] and colors[ind] != 0:
                     cp -=1
-                array[i] = j
-                if array[i] == array[i+1]:
+                colors[ind] = val
+                if colors[ind] == colors[ind+1] and colors[ind] != 0:
                     cp +=1
-            elif i == n-1:
-                if array[i-1] == array[i] and (array[i] != 0):
+                ans.append(cp)
+            elif ind == n-1:
+                if colors[ind] == colors[ind-1] and colors[ind] != 0:
                     cp -=1
-                array[i] = j
-                if array[i] == array[i-1]:
+                colors[ind] = val
+                if colors[ind] == colors[ind-1] and colors[ind] != 0:
                     cp +=1
+                ans.append(cp)
             else:
-                if array[i+1] == array[i] and (array[i] != 0):
+                if colors[ind] == colors[ind-1] and colors[ind] != 0:
                     cp -=1
-                if array[i-1] == array[i] and (array[i] != 0):
+                if colors[ind] == colors[ind+1] and colors[ind] != 0:
                     cp -=1
-                array[i] = j
-                if array[i] == array[i-1]:
+                
+                colors[ind] = val
+                if colors[ind] == colors[ind-1] and colors[ind] != 0:
                     cp +=1
-                if array[i] == array[i+1]:
+                if colors[ind] == colors[ind+1] and colors[ind] != 0:
                     cp +=1
-
-            ans.append(cp)
-
-            # ans.append()
+                ans.append(cp)
         return ans
