bc992b6fd5897c4d2f88842b4e2c413cffc3e129
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2025-05-22 02:27:30 +0300

    Time: 575 ms (60.84%), Space: 20.7 MB (69.06%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,26 +1,29 @@
-from collections import Counter
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        cp = Counter(nums)
         nums.sort()
-        a = 0
-        n = len(nums)-1
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i-1]:
-                continue
-            l = i+1
-            r = len(nums)-1
-            while l<r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r-=1
-                elif threeSum < 0:
-                    l+=1
+        res = []
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue  # avoid duplicates
+
+            left, right = i + 1, len(nums) - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
                 else:
-                    ans.append([a, nums[l], nums[r]])
-                    l += 1
-                    while nums[l] == nums[l-1] and l<r:
-                        l+=1
-        return ans
+                    res.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+
+                    # skip duplicates
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+        return res
