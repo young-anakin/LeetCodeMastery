da51b2aae1b9c0389b7dce58f0f7ccd2e16ddde2
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2025-07-25 18:28:20 +0300

    Time: 259 ms (41.21%), Space: 20.3 MB (57.52%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,34 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        # directions to go
-        dirs = [(0, 1), (1, 0), (-1, 0), (0, -1)]
-
-        # inbound checker
-
-        def inbound(ind, j):
-            return ind >= 0 and j >=0 and ind < len(grid) and j < len(grid[0])
         
-        visited = set()
-        islands = 0
-        def islandSizeCalculator(ind , j):
-            queue = deque()
-            queue.append((ind, j))
-            visited.add((ind, j))
-            while queue:
-                ind, j = queue.popleft()
-                for horiz, vert in dirs:
-                    new_i, new_j = ind + horiz, j + vert
-                    if inbound(new_i, new_j) and (new_i, new_j) not in visited and grid[new_i][new_j] == "1":
-                        queue.append((new_i, new_j))
-                        visited.add((new_i, new_j))
+        # represents four directions of a point
+        dir = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
-        island = 0
+        # function to check wether a current point is inbound or out of bound
+        def inbound(i, j):
+            return i >= 0 and j >= 0 and i < len(grid) and j < len(grid[0])
+
+        def traversal(i, j):
+            grid[i][j] = "0"
+            for a, b in dir:
+                newi, newj = a +i , j + b
+                if inbound(newi, newj) and grid[newi][newj] == "1":
+                    traversal(newi, newj)
+        # Grid Traversal
+        islandCount = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1" and (i, j ) not in visited:
-                    island +=1
-                    islandSizeCalculator(i, j)
+                if grid[i][j] == "1":
+                    traversal(i, j)
+                    islandCount +=1
+        
+        # BFS - Breadth first search. queue 
 
-        return island
-                    
+        
+
+        return islandCount
+
+            
