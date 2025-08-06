056dd8e4a184f7e5b983ab0a57fefe9dd1c05843
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2025-08-06 19:28:11 +0300

    Time: 3 ms (81.75%), Space: 17.8 MB (71.69%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,28 +1,34 @@
 class Solution:
-    def orangesRotting(self, orange: List[List[int]]) -> int:
-        visited = set()
-        grid = [(1,0), (-1,0), (0,1), (0,-1)]
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        
+
+        dir = [(1, 0), (0, -1), (-1, 0), (0,1)]
+
         def inbound(i, j):
-            return i >= 0 and j >= 0 and i < len(orange) and j < len(orange[0])
+            return i >= 0 and j>=0 and i < len(grid) and j < len(grid[0])
+
         
         queue = deque()
-        for i in range(len(orange)):
-            for j in range(len(orange[0])):
-                if orange[i][j] == 2:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 2:
                     queue.append(((i, j), 0))
-        cp = 0
+                    grid[i][j] = 0
+
+        mx = 0
         while queue:
-            a, b = queue.popleft()
-            for level in grid:
-                i, j = a[0] + level[0], a[1] + level[1]
-                if inbound(i, j) and orange[i][j] == 1:
-                    orange[i][j] = 0
-                    queue.append(((i, j), b+1))
-                    cp = max(cp, b+1)
+            coord,  time = queue.popleft()
+            for xi, xj in dir:
+                newi, newj = coord[0] + xi, coord[1] + xj
+                if inbound(newi, newj) and grid[newi][newj] == 1:
+                    queue.append(((newi, newj), time+1))
+                    grid[newi][newj] = 0
+                    mx = max(time+1, mx)
         
-        for i in range(len(orange)):
-            for j in range(len(orange[0])):
-                if orange[i][j] == 1:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
                     return -1
-        return cp
+        return mx
+
 
