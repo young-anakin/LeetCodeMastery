8b45c7498527755dc752d2eee80048399e958cd0
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2025-08-08 19:22:23 +0300

    Time: 146 ms (48.5%), Space: 31.7 MB (98.49%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -1,41 +1,18 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
+        maxx = -float("inf")
+        good = defaultdict(int)
 
-        queue = deque()
-
-        cp = 0
-        queue.append((root, root.val))
-
-        # stack 
-        # recursive
-
-        def dfs(node, curr_max):
-            nonlocal cp
+        def check_goodness(node, maxx, good):
             if node == None:
                 return
-            if curr_max <= node.val:
-                cp +=1
-            
-            dfs(node.left, max(curr_max, node.val))
-            dfs(node.right, max(curr_max, node.val))
+            if maxx <= node.val:
+                print("We found gold!")
+                good[0] += 1
+                maxx = node.val
+            check_goodness(node.left, maxx, good)
+            check_goodness(node.right, maxx, good)
         
-        dfs(root, root.val)
-        return cp
-        # while queue:
-        #     node, curr_max = queue.popleft()
-        #     if node == None:
-        #         continue
-        #     if curr_max <= node.val:
-        #         cp +=1
-            
-        #     queue.append((node.left, max(curr_max, node.val)))
-        #     queue.append((node.right, max(curr_max, node.val)))
-        
-        # return cp
-        
+        check_goodness(root, maxx, good)
+
+        return good[0]
