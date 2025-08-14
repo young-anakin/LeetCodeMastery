b8aadaf9862d582e76746a73fd1a01d0975c1233
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2025-08-14 18:40:40 +0300

    Time: 7 ms (26.65%), Space: 19.2 MB (49.09%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,32 +1,33 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # a, b inorder to take course a, you have to take course b
-
-        dependency = defaultdict(int)
-        graph = defaultdict(list)
-
+        
+        dependents = defaultdict(list)
+        dependencyAmount = defaultdict(int)
         for a, b in prerequisites:
-            graph[b].append(a)
-            dependency[a] +=1
+            dependents[b].append(a)
+            dependencyAmount[a] +=1
         
-        queue = deque()
-        ans = []
-        visited = set()
+        print(dependents)
+        independent = deque()
+        ans = list()
         for i in range(numCourses):
-            if dependency[i] == 0:
-                queue.append(i)
-                visited.add(i)
+            if dependencyAmount[i] == 0:
+                independent.append(i)
                 ans.append(i)
-        # visited = set()
-        while queue:
-            val = queue.popleft()
-            for i in graph[val]:
-                dependency[i] -=1
-                if dependency[i] == 0:
-                    visited.add(i)
-                    queue.append(i)
-                    ans.append(i)
         
-        if len(visited) == numCourses:
-            return ans
-        return []
+
+        completed = len(independent)
+        while independent:
+            val = independent.popleft()
+            for ind in dependents[val]:
+                dependencyAmount[ind] -=1
+                if dependencyAmount[ind] == 0:
+                    independent.append(ind)
+                    ans.append(ind)
+        
+        if len(ans) != numCourses:
+            return []
+        return ans
+
+
+
