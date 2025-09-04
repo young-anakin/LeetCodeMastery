b852b0f991936925aa6c26c027b7784b1dd455df
Micheal G. Zeleke <88850718+young-anakin@users.noreply.github.com>
2025-09-04 20:08:25 +0300

    Time: 1109 ms (12.18%), Space: 414.7 MB (5.13%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+
+
+        # Knapsack ( one turn , we take . Another turn, we leave) -> Take it or leave it
+        ans = 0
+
+        @cache
+        def dp(ind, curr_amount):
+            # Base case
+
+            if curr_amount > amount:
+                return 0
+            if ind >= len(coins):
+                return 0
+            
+            if curr_amount == amount:
+                return 1
+            
+            return dp(ind, curr_amount + coins[ind]) + dp(ind+1, curr_amount) 
+
+
+        return dp(0, 0)
